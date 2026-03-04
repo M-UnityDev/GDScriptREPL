67e0cac5e4f770f993a4437642e9dfb1f0eae044
M-Unity <mihailsheshukov10@gmail.com>
2026-03-04 18:42:51 +0500

    Static Typing
    Signed-off-by: M-Unity <mihailsheshukov10@gmail.com>

--- repl.gd
+++ repl.gd
@@ -1,35 +1,35 @@
 extends Node
 
 class colors:
-	static var BLACK = "\u001b[0;30m"
-	static var RED = "\u001b[0;31m"
-	static var GREEN = "\u001b[0;32m"
-	static var BROWN = "\u001b[0;33m"
-	static var BLUE = "\u001b[0;34m"
-	static var PURPLE = "\u001b[0;35m"
-	static var CYAN = "\u001b[0;36m"
-	static var LIGHT_GRAY = "\u001b[0;37m"
-	static var DARK_GRAY = "\u001b[1;30m"
-	static var LIGHT_RED = "\u001b[1;31m"
-	static var LIGHT_GREEN = "\u001b[1;32m"
-	static var YELLOW = "\u001b[1;33m"
-	static var LIGHT_BLUE = "\u001b[1;34m"
-	static var LIGHT_PURPLE = "\u001b[1;35m"
-	static var LIGHT_CYAN = "\u001b[1;36m"
-	static var LIGHT_WHITE = "\u001b[1;37m"
-	static var BOLD = "\u001b[1m"
-	static var FAINT = "\u001b[2m"
-	static var ITALIC = "\u001b[3m"
-	static var UNDERLINE = "\u001b[4m"
-	static var BLINK = "\u001b[5m"
-	static var NEGATIVE = "\u001b[7m"
-	static var CROSSED = "\u001b[9m"
-	static var END = "\u001b[0m"
-	static var CLEAR = "\u001b[H\u001b[2J"
-	static var DELETE = "\u001b[P"
+	static var BLACK: String = "\u001b[0;30m"
+	static var RED: String = "\u001b[0;31m"
+	static var GREEN: String = "\u001b[0;32m"
+	static var BROWN: String = "\u001b[0;33m"
+	static var BLUE: String = "\u001b[0;34m"
+	static var PURPLE: String = "\u001b[0;35m"
+	static var CYAN: String = "\u001b[0;36m"
+	static var LIGHT_GRAY: String = "\u001b[0;37m"
+	static var DARK_GRAY: String = "\u001b[1;30m"
+	static var LIGHT_RED: String = "\u001b[1;31m"
+	static var LIGHT_GREEN: String = "\u001b[1;32m"
+	static var YELLOW: String = "\u001b[1;33m"
+	static var LIGHT_BLUE: String = "\u001b[1;34m"
+	static var LIGHT_PURPLE: String = "\u001b[1;35m"
+	static var LIGHT_CYAN: String = "\u001b[1;36m"
+	static var LIGHT_WHITE: String = "\u001b[1;37m"
+	static var BOLD: String = "\u001b[1m"
+	static var FAINT: String = "\u001b[2m"
+	static var ITALIC: String = "\u001b[3m"
+	static var UNDERLINE: String = "\u001b[4m"
+	static var BLINK: String = "\u001b[5m"
+	static var NEGATIVE: String = "\u001b[7m"
+	static var CROSSED: String = "\u001b[9m"
+	static var END: String = "\u001b[0m"
+	static var CLEAR: String = "\u001b[H\u001b[2J"
+	static var DELETE: String = "\u001b[P"
 
 var temp_line: String
-var script_instance
+var script_instance: Node
 var file: String
 var HELP := "Welcome to GDScREPL - Unoffical REPL for GDScript
 
@@ -44,7 +44,7 @@ func _ready() -> void:
 		await run_script_from_string(FileAccess.open(file, FileAccess.READ).get_as_text())
 		get_tree().quit(0)
 		return
-	var GODOT_VERSION = str(Engine.get_version_info()["major"]) + "." + str(Engine.get_version_info()["minor"]) + "." + str(Engine.get_version_info()["patch"])
+	var GODOT_VERSION := str(Engine.get_version_info()["major"]) + "." + str(Engine.get_version_info()["minor"]) + "." + str(Engine.get_version_info()["patch"])
 	print("GDScript " + GODOT_VERSION + " (" + Time.get_date_string_from_system(true) + " " + Time.get_time_string_from_system(true) + ") on " + OS.get_name() + 
 			"\nType \"help\" for more information.")
 	while(true):
@@ -65,7 +65,7 @@ func _ready() -> void:
 		temp_line = ""
 
 func run_script_from_string(code: String) -> bool:
-	var script = GDScript.new()
+	var script := GDScript.new()
 	script.set_source_code(code)
 	if script.reload() != OK:
 		return true
